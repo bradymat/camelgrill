68aefad465548630a5616ef0ff25236a05ed38a7
bradymat <bradymat@gmail.com>
2018-07-13 04:37:34 +1200

    fix mobile bug being halfway down page

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,10 @@
 import html from 'yo-yo'
 
-module.exports = ({ title }, dispatch) => html`
+module.exports = ({ title }, dispatch) => {
+  window.scrollTo(0, 0);
+  return html`
 <footer id="footer">
   <p class="copyright">Developer: <a href="http://bradymat.com">bradymat</a></p>
 </footer>
 `
+}
