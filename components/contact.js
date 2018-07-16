ea665e6de287b2ae2d78b176932b5154d6853dc3
bradymat <bradymat@gmail.com>
2018-07-17 03:32:00 +1200

    change contact email > messenger

--- components/contact.js
+++ components/contact.js
@@ -9,14 +9,14 @@ module.exports = (state, dispatch) => html`
       <article>
       ${back(dispatch)}
       <h1 class="major">Contact</h1>
-      <p>We'd love to hear from you! To get in touch or request catering email Abdalla at <a href="mailto:abdol4gud@gmail.com" target="_">abdol4gud@gmail.com</a> or call us on <span><a href="tel:+6443846813">04-384 6813</a></span></p>
+      <p>We'd love to hear from you! To get in touch or request catering message Camel Grill on <a href="https://www.facebook.com/pg/CamelGrillNZ" target="_">Facebook</a> or call us on <span><a href="tel:+6443846813">04-384 6813</a></span></p>
       <p>Like our Facebook page to stay up to date with our latest promotions and special menu items.</p>
       <span id="likeButton" >
       <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FCamelGrillNZ%2F&width=63&layout=button&action=like&size=large&show_faces=false&share=false&height=65&appId" width="63" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
       </span>
       <ul class="icons">
         <li><a href="https://www.facebook.com/pg/CamelGrillNZ" target="_" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-        <li><a href="mailto:abdol4gud@gmail.com" target="_" class="icon fa-envelope"><span class="label">Mail</span></a></li>
+        <li><a href="https://www.google.co.nz/maps/place/Camel+Grill/@-41.2920375,174.7750122,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd7471559d5:0xbb9b6a62c1b06ee4!8m2!3d-41.2920375!4d174.7772009" target="_" class="icon fa-map-marker"><span class="label">Address</span></a></li>
         <li><a href="tel:+6443846813" target="_" class="icon fa-phone"><span class="label">Call</span></a></li>
       </ul>
       <div id="map" class="map-responsive">
